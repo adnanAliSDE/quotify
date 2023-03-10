c975ed3370abcb79112b70203fec0e64b50261f9
Adnan Ali <112294571+adnanAliSDE@users.noreply.github.com>
2023-03-10 12:11:13 +0530

    fixed auto populating issue

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const slider = document.querySelector(".slider");
 
 // generating random number
 const random = () => {
-  return Math.floor(Math.random() * 1000 + 643);
+  const temp = Math.floor(Math.random() * 1000 + 643); // Need to be removed
+  return temp;
 };
 
 // fetching data
@@ -18,25 +19,40 @@ fetch("https://type.fit/api/quotes")
   .then(function (data) {
     quotes = data;
     populate();
+    console.log("API call called populate");
   });
+
+
+let delay = 5000; //new quote delay duration - 30s
+
+//Interval to call get change the current quote
+let popInterval; //
+let populateAnimationTimer;// timeout to be called when animation is resumed
+
 
 // Populating the dom
 const populate = () => {
+  clearInterval(popInterval)
+  popInterval = setInterval(populate, delay)
+  while (populateAnimationTimer) {
+    populateAnimationTimer = null
+    window.clearTimeout(populateAnimationTimer); // will do nothing if no timeout with id is present
+  }
   let quote = quotes[random()];
   textContainer.innerHTML = `"${quote.text}"`;
   authorContainer.innerHTML = quote.author;
   slider.getAnimations()[0].currentTime = 0;
 };
-
-// getting new quotes
-let delay = 30000; //new quote delay duration
+// getting next quote
 reloadBtn.addEventListener("click", () => {
+  if (
+    slider.style.animationPlayState == "paused"
+  ) {
+    slider.style.animationPlayState = "running";
+  }
   populate();
-  clearInterval(popInterval);
-  popInterval = setInterval(populate, delay);
-});
+})
 
-const popInterval = setInterval(populate, delay);
 
 // pausing the current quote
 pauseBtn.addEventListener("click", () => {
@@ -45,12 +61,14 @@ pauseBtn.addEventListener("click", () => {
     slider.style.animationPlayState == ""
   ) {
     slider.style.animationPlayState = "paused";
-    clearInterval(popInterval);
+    clearInterval(popInterval)
   } else {
     slider.style.animationPlayState = "running";
-    setTimeout(() => {
+    console.log("Time left: ", delay - slider.getAnimations()[0].currentTime);
+    // This portion contains bug when populate is called these settimeout and the 
+    populateAnimationTimer = setTimeout(() => {
       populate();
-      popInterval = setInterval(populate, delay);
+      console.log("Animation settimeout called populate");
     }, delay - slider.getAnimations()[0].currentTime);
   }
 });
